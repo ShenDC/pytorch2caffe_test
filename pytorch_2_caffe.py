f5e5de1e6e7cf554cfa9307052cbb110c3d0837b
ShenDC <33150874+ShenDC@users.noreply.github.com>
2020-07-21 09:52:15 +0800

    Update pytorch_2_caffe.py

--- pytorch_2_caffe.py
+++ pytorch_2_caffe.py
@@ -8,36 +8,7 @@ def module_hook(module, input, output):
 
     name = layer_name_dict[module]
     # try to trans pytorch to caffe or other freamwork model
-    if isinstance(module, nn.Conv2d):
-        out = Conv(input[0], module.kernel_size, module.out_channels,
-                   module.stride, module.padding, group_size=module.groups, name=name)
-    elif isinstance(module, nn.ConvTranspose2d):
-        out = Conv(input[0], module.kernel_size, module.out_channels,
-                   module.stride, module.padding, group_size=module.groups, name=name, transpose=True)
-    elif isinstance(module, nn.BatchNorm2d):
-        out = Norm(input[0], 'batch_norm', name=name)
-    elif isinstance(module, nn.Linear):
-        out = fc(input[0], module.out_features, name=name)
-    elif isinstance(module, nn.MaxPool2d):
-        out = pool(input[0], module.kernel_size, module.stride, module.padding,
-                   name=name, pool_type='max')
-    elif isinstance(module, nn.AvgPool2d):
-        out = pool(input[0], module.kernel_size, module.stride, module.padding,
-                   name=name, pool_type='avg')
-    elif isinstance(module, nn.ReLU):
-        out = Activation(input[0], 'relu', name=name)
-    elif isinstance(module, nn.LeakyReLU):
-        out = Activation(input[0], 'relu', name=name)
-    elif isinstance(module, nn.Sigmoid):
-        out = Activation(input[0], 'sigmoid', name=name)
-    elif isinstance(module, nn.Conv3d):
-        out = Conv(input[0], module.kernel_size, module.out_channels,
-                   module.stride, module.padding, group_size=module.groups, name=name)
-
-    if out:
-        pass
-    else:
-        print('WARNING: skip Module {}'.format(module))
+   
 
 def trans(net, input):
 
